8aa12b03e61a14e480ccd68c81b5838456305a88
minabadrous <minabadrous.vu@gmail.com>
2023-07-23 16:17:29 +0300

    Express, ejs set up

---
+++ routes/shop.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const adminData = require("./admin");
+
+const router = express.Router();
+
+router.get("/", (_req, res) => {
+    const products = adminData.products;
+    res.render("shop", {
+        prods: products,
+        pageTitle: "Shop",
+        path: "/",
+    });
+});
+
+module.exports = router;
